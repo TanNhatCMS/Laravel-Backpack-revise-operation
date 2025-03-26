04eeef8fb170745bfaf1ff9fc3d58e1179b74bf8
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2025-03-27 06:50:26 +0700

    add language vietnamese

---
+++ resources/lang/vi/revise.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backpack Revise Operation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the CRUD interface.
+    | You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    // Revisions
+    'revisions' => 'Các phiên bản sửa đổi',
+    'no_revisions' => 'Không tìm thấy phiên bản sửa đổi nào',
+    'revisions_for' => 'Các phiên bản sửa đổi cho',
+    'created_this' => 'đã tạo cái này',
+    'changed_the' => 'đã thay đổi',
+    'restore_this_value' => 'Khôi phục giá trị này',
+    'from' => 'từ',
+    'to' => 'đến',
+    'undo' => 'Hoàn tác',
+    'revision_restored' => 'Đã khôi phục thành công phiên bản sửa đổi',
+    'guest_user' => 'Người dùng khách',
+];
